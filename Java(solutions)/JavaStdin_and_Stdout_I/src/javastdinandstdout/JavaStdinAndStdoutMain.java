04f443944ba6827e484af61a3d573f57c355defc
Andreea-Maria Drăghici <72825756+AndreeaDraghici@users.noreply.github.com>
2021-03-29 17:15:34 +0300

    Update JavaStdinAndStdoutMain.java

--- Java(solutions)/JavaStdin_and_Stdout_I/src/javastdinandstdout/JavaStdinAndStdoutMain.java
+++ Java(solutions)/JavaStdin_and_Stdout_I/src/javastdinandstdout/JavaStdinAndStdoutMain.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class JavaStdinAndStdoutMain {
 
 	public static void main(String[] args) {
-	try (
-	Scanner scan = new Scanner(System.in)) {
+   try {
+	      Scanner scan = new Scanner(System.in)) {
 		int a = scan.nextInt();
 		// Complete this line
 		int b = scan.nextInt();
@@ -20,7 +20,10 @@ public class JavaStdinAndStdoutMain {
 		
 		// Complete this line
 		 System.out.println(c);
-	}
+	}catch(Exception e)
+		{
+	            System.out.println(e.getMessage());
+		}
 	       
   }
 
