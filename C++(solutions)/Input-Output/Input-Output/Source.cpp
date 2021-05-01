b88db810f183c78186e0c1bfaeb461066c1c7121
Andreea-Maria Drăghici <72825756+AndreeaDraghici@users.noreply.github.com>
2021-05-01 12:05:59 +0300

    Add files via upload

---
+++ C++(solutions)/Input-Output/Input-Output/Source.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a, b, c, suma = 0;
+    cin >> a >> b >> c;
+    suma = a + b + c;
+    cout << suma;
+    return 0;
+}
